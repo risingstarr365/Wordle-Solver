b4eac911e6ed8e8c407ec5128f21f72979b3b681
Aayush <139818684+risingstarr365@users.noreply.github.com>
2025-04-10 22:29:26 -0400

    Create _init_.py

---
+++ _init_.py
@@ -0,0 +1,10 @@
+"""
+Wordle Solver Package
+
+A Python package for solving Wordle puzzles using a systematic approach.
+"""
+
+from .wordle_solver import WordleSolver
+
+__version__ = "0.1.0"
+__all__ = ["WordleSolver"] 
